0f5c9e3d176d16f08b21f3364c256faeb0e0827d
nazm0l <tanjirahmednazmul@gmail.com>
2022-04-08 03:30:41 +0600

    firebase and form added

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDFxdlrswkWSHUZkvLoQt_pTzm0EQoCkx8",
+  authDomain: "login-form-b6baf.firebaseapp.com",
+  projectId: "login-form-b6baf",
+  storageBucket: "login-form-b6baf.appspot.com",
+  messagingSenderId: "589544468205",
+  appId: "1:589544468205:web:ccc51b244747e40c5066e4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
